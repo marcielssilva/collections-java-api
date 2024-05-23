87781dd5230608330f25b4a75f15ddef24859c03
marcielssilva <marciel.ssilva2@gmail.com>
2024-05-23 12:09:25 -0300

    Resolvendo Exercicios Map - Pesquisa
    Signed-off-by: marcielssilva <marciel.ssilva2@gmail.com>

---
+++ src/main/java/map/Pesquisa/EstoqueProduto/Produto.java
@@ -0,0 +1,36 @@
+package main.java.map.Pesquisa.EstoqueProduto;
+
+public class Produto {
+    //atributo
+    private String nome;
+    private int quantidade;
+    private double preco;
+
+    public Produto(String nome, int quantidade, double preco) {
+
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                ", nome='" + nome + '\'' +
+                ", quantidade=" + quantidade +
+                ", preco=" + preco +
+                '}';
+    }
+}
